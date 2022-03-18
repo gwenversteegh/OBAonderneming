9d773bdad362a23e00a6560f1b8251fb28f3f61a
gwenversteegh <70900763+gwenversteegh@users.noreply.github.com>
2022-03-18 11:08:33 +0100

    fjdkfsjfl

--- script.js
+++ script.js
@@ -10,7 +10,6 @@ zoeken()
 sectionInViewport()
 sectionInViewportBack()
 
-// const url = 'https://zoeken.oba.nl/api/v1/search/?q=%22voeding%22&refine=true&authorization=16c19e6083308c984c452600134989ba&sort=act_dt_asc&output=json' //&branch=OBA%20geuzenveld'
 
 
 fetch(url)
